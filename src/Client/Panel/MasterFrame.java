a68a6b22f83ba8e7a0f64794ffdb41b7c5a12573
Martin Harrysson <MartinHarrysson1990@hotmail.com>
2023-11-21 13:01:48 +0100

    added connection to server

--- src/Client/Panel/MasterFrame.java
+++ src/Client/Panel/MasterFrame.java
@@ -2,8 +2,11 @@ package Client.Panel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MasterFrame extends JFrame {
     private final CardLayout layout = new CardLayout();
@@ -11,12 +14,38 @@ public class MasterFrame extends JFrame {
     String pageNumber = "login";
     ObjectOutputStream sendToServer;
     ObjectInputStream fromServer;
+    Socket socketToServer;
 
 
     public MasterFrame(){
         add(allPanels);
 
         showPage(pageNumber);
+
+        try {
+            socketToServer = new Socket("127.0.0.1", 6666);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ObjectOutputStream out = new ObjectOutputStream(socketToServer.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socketToServer.getInputStream())
+        ){
+            sendToServer = out;
+            fromServer = in;
+            out.writeObject("Player1");
+
+            Object objectFromServer = in.readObject();
+            System.out.println((String) objectFromServer);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(new Dimension(500,520));
