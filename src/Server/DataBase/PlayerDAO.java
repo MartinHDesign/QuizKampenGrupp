300c1fee2da57cc7f35de18930d2700ab860d937
r0wm0e <rallwrw@hotmail.com>
2023-11-29 12:02:26 +0100

    added Playerlist.txt cleaned some code

--- src/Server/DataBase/PlayerDAO.java
+++ src/Server/DataBase/PlayerDAO.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerDAO {
-    private List<Player> playerDAO = new ArrayList<>(); // kalle,0
+    private List<Player> playerDAO = new ArrayList<>();
 
 
     public void writeNewPlayerToFile(List<Player>updatedPlayerDAO) {
@@ -32,12 +32,9 @@ public class PlayerDAO {
             while ((line = readFromFile.readLine()) != null) {
                 String[] playerListData = line.split(",");
                 String userName = playerListData[0];
-                System.out.println(userName);
                 int highScore = Integer.parseInt(playerListData[1]);
-                System.out.println(highScore);
 
-                //första linen som läses in = martin,8
-                Player temp = new Player(userName,highScore,null); // username, highscore
+                Player temp = new Player(userName,highScore,null);
                 playerDAO.add(temp);
             }
 
@@ -49,28 +46,4 @@ public class PlayerDAO {
         }
     }
 
-    public static void main(String[] args) {
-        PlayerDAO playerDAO = new PlayerDAO();
-        playerDAO.readPlayerFromFile();
-
-
-        List<Player> updatedPlayerDAO = new ArrayList<>();
-        Player newPlayer = new Player("username",10,null);
-        Player newPlayer2 = new Player("username2",10,null);
-        Player newPlayer3 = new Player("username3",10,null);
-        Player newPlayer4 = new Player("username4",10,null);
-        updatedPlayerDAO.add(newPlayer);
-        updatedPlayerDAO.add(newPlayer2);
-        updatedPlayerDAO.add(newPlayer3);
-        updatedPlayerDAO.add(newPlayer4);
-
-        playerDAO.writeNewPlayerToFile(updatedPlayerDAO);
-
-
-
-        for(Player player : playerDAO.playerDAO) {
-            System.out.println(player.getName() + player.getHighScore());
-        }
-
-    }
 }
